b6c71b7a73c0289191e231e99e867de6d083a7fc
Anuraj <anuraj.p@msn.com>
2020-12-21 12:32:18 +0530

    Google Removed.

--- api/Startup.cs
+++ api/Startup.cs
@@ -1,8 +1,4 @@
-using System;
-using Google.Apis.Safebrowsing.v4;
-using Google.Apis.Services;
 using Microsoft.Azure.Functions.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
 
 [assembly: FunctionsStartup(typeof(DotnetThoughts.Web.Startup))]
 
